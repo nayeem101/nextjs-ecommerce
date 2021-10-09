2849be383b5720b0616d82d4cb390a5bf2b87899
Saiful Islam <saiful70.me@gmail.com>
2021-10-09 20:00:04 +0600

    created login system with help of formik

---
+++ pages/auth/signup.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navfull from "../../Components/Navbar";
+
+import Auth from "../../Components/Auth";
+
+const login = () => {
+   return (
+      <>
+         <Navfull />
+
+         {/* <Auth /> */}
+      </>
+   );
+};
+
+export default login;
